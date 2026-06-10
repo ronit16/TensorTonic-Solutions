980d88986e990558698051fbbab02bb182c902f8
ronit16 <124552484+ronit16@users.noreply.github.com>
2026-06-10 16:06:15 +0530

    Solve swish-activation

---
+++ swish-activation/swish-activation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def swish(x):
+    """
+    Implement Swish activation function.
+    """
+    # Write code here
+    x = np.array(x, dtype=float)
+
+    sigmoid = 1 / (1 + np.exp(-x))
+
+    return x * sigmoid
